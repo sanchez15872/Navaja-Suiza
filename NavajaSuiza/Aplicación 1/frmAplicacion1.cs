9ec6caa7f5f4d987a8ffe0bafad18a35e32e345c
sanchez15872 <sanchez15872@iesmarenostrum.com>
2017-03-03 14:12:29 +0100

    Prueba

---
+++ NavajaSuiza/Aplicación 1/frmAplicacion1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavajaSuiza.Aplicación_1
+{
+    public partial class frmAplicacion1 : Form
+    {
+        /// <summary>
+        /// crea una variable de tipo aplicacion1 y la instancia para uso posterior
+        /// </summary>
+        Aplicacion1 oAplicacion1=new Aplicacion1();
+
+        public frmAplicacion1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        /// <summary>
+        /// evento que lanza un método de la clase aplicación1
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(oAplicacion1.Metodo());
+        }
+    }
+}
---
+++ NavajaSuiza/frmMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavajaSuiza
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// llama a una aplicación
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aplicación_1.frmAplicacion1 oFormulario = new Aplicación_1.frmAplicacion1();
+            oFormulario.ShowDialog();
+
+            
+         }
+    }
+}
